b005dfc455f5bb32124e131986313acc1e59c4bb
David Vílchez <dvilchez@xuaps.com>
2024-09-14 12:21:12 +0200

    Refactor code

--- tests/test_goal_tracker.py
+++ tests/test_goal_tracker.py
@@ -1,4 +1,4 @@
-class Goal:
+class Tracking:
     def __init__(self, user_id, product_id, goal):
         self.user_id = user_id
         self.product_id = product_id
@@ -9,18 +9,18 @@ class GoalChecker:
     def __init__(self, prices):
         self.prices = prices
 
-    def check_goal(self, goal):
-        price = self.prices.get_price(goal.product_id)
-        return price < goal.goal
+    def check_goal(self, tracking):
+        price = self.prices.get_price(tracking.product_id)
+        return price < tracking.goal
 
 
-def test_goal_checker():
+def test_check_goal():
     class MockPrices:
         def get_price(self, product_id):
             return 2
 
     prices = MockPrices()
-    goal = Goal("any_user", "any_product", 3)
+    tracking = Tracking("any_user", "any_product", 3)
     checker = GoalChecker(prices)
 
-    assert checker.check_goal(goal) is True
+    assert checker.check_goal(tracking) is True
